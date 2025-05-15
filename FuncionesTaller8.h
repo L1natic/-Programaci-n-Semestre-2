d84875bd7073e623c46e0f53a57b633660b3ba50
Jaime Velastegui <jaime.velastegui@udla.edu.ec>
2025-05-15 15:15:00 -0500

    Add files via upload

--- FuncionesTaller8.h
+++ FuncionesTaller8.h
@@ -14,6 +14,8 @@ struct Producto {
 int pedirCantidad();
 void ingresarProductos(struct Producto productos[], int cantidad);
 void mostrarProductos(struct Producto productos[], int cantidad);
+void buscarProducto(struct Producto productos[], int cantidad);
+void EditarNombre(struct Producto productos[], int cantidad);
 
 
 // Definiciones de funciones
@@ -38,9 +40,10 @@ void ingresarProductos(struct Producto productos[], int cantidad) {
     for (int i = 0; i < cantidad; i++) {
         printf("\nProducto %d\n", i + 1);
         printf("Nombre: ");
+        getchar(); // Limpiar el buffer
         fgets(productos[i].nombre, sizeof(productos[i].nombre), stdin);
-        productos[i].nombre[strcspn(productos[i].nombre, "\n")] = '\0'; // Eliminar el salto de línea
         printf("Precio: $ ");
+        fflush(stdin);
         scanf("%f", &productos[i].precio);
         printf("Codigo: ");
         scanf("%d", &productos[i].codigo);
@@ -56,6 +59,58 @@ void mostrarProductos (struct Producto productos[], int cantidad) {
     printf("\n--- Lista de Productos ---\n");
     for (int i = 0; i < cantidad; i++) {
         printf("%d. %s | Precio: %.2f | Codigo: %d | Cantidad: %d | Tiempo/u: %.2f h\n", i + 1, productos[i].nombre, productos[i].precio, productos[i].codigo, productos[i].cantidad, productos[i].tiempoFabricacion);
+    }
+}
+
+
+void buscarProducto(struct Producto productos[], int cantidad){
+    int codigo, opc2;
+    printf("Si desea buscar por codigo presione 1 y si desea buscar por nombre presione 2: ");
+    scanf("%d", &opc2);
+    fflush(stdin);
+    if (opc2 == 1) {
+        printf("Ingrese el codigo del producto a buscar: ");
+        scanf("%d", &codigo);
+        fflush(stdin);
+        for (int i = 0; i < cantidad; i++) {
+            if (productos[i].codigo == codigo) {
+                printf("Producto encontrado: %s | Precio: %.2f\n", productos[i].nombre, productos[i].precio);
+                return;
+            }
+        }
+    } else if (opc2 == 2) {
+        printf("Ingrese el nombre del producto a buscar: ");
+        fflush (stdin); // Limpiar el buffer
+        int encontrado = 0;
+        char nombreBusqueda[50];
+        fgets(nombreBusqueda, sizeof(nombreBusqueda), stdin);
+        nombreBusqueda[strcspn(nombreBusqueda, "\n")] = '\0';
+        for (int i = 0; i < cantidad; i++) {
+            if (strcmp(nombreBusqueda, productos[i].nombre) == 0) {
+                printf("Producto encontrado: %s | Precio: %.2f\n", productos[i].nombre, productos[i].precio);
+                encontrado = 1;
+                break;
+            }
+        }
+    } else {
+        printf("Opcion invalida.\n");
+        return;
+    }
+}
+
+void EditarNombre (struct Producto productos[], int cantidad) {
+    int code;
+    printf("Ingrese el codigo del producto a editar: ");
+    fflush(stdin);
+    scanf("%d", &code);
+    for (int i = 0; i < cantidad; i++) {
+        if (productos[i].codigo == code) {
+            printf("Ingrese el nuevo nombre del producto: ");
+            fflush(stdin); // Limpiar el buffer
+            fgets (productos[i].nombre, sizeof(productos[i].nombre), stdin);
+            printf("Nombre actualizado a: %s\n", productos[i].nombre);
+            return;
+        }
     }
 }
 
--- TallerSemana8.c
+++ TallerSemana8.c
@@ -26,7 +26,7 @@ int main() {
         printf("\n--- Menu ---\n");
         printf("1. Buscar producto por codigo\n");
         printf("2. Mostrar todos los productos\n");
-        printf("3. Editar nombre del producto\n");
+        printf("3. Editar nombre del producto por codigo\n");
         printf("4. Editar cantidades\n");
         printf("5. Editar tiempo de fabricacion por codigo\n");
         printf("6. Verificar si cumple con la demanda (horas - recursos)\n");
@@ -35,7 +35,24 @@ int main() {
         scanf("%d", &opcion);
         fflush(stdin);
 
-    } while (opcion != 6);
+        switch (opcion) {
+            case 1:
+                buscarProducto(productos, cantidad);
+                break;
+
+            case 2:
+                mostrarProductos(productos, cantidad);
+                break;
+
+            case 3:
+                EditarNombre(productos, cantidad);
+                break;
+                
+            default:
+                printf("Opcion no valida. Intente de nuevo.\n");
+                break;
+        }
+    } while (opcion != 7);
 
     return 0;
 }
